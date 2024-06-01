944c2e9e7ef56e1cf17304ba7d9617c04642f76a
kshitij_chitransh1910 <98554008+lucifer00700@users.noreply.github.com>
2024-06-01 12:04:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Even from './pages/even';
+import Fibo from './pages/fibo';
+import Prime from './pages/prime';
+import Rand from './pages/rand';
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/even"><button>Even Numbers</button></Link>
+          </li>
+          <li>
+            <Link to="/fibo"><button>Fibonacci Sequence</button></Link>
+          </li>
+          <li>
+            <Link to="/prime"><button>Prime Numbers</button></Link>
+          </li>
+          <li>
+            <Link to="/rand"><button>Random Numbers</button></Link>
+          </li>
+        </ul>
+      </nav>
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/even" element={<Even />} />
+        <Route path="/fibo" element={<Fibo />} />
+        <Route path="/prime" element={<Prime />} />
+        <Route path="/rand" element={<Rand />} />
+      </Routes>
+    </Router>
   );
 }
 
